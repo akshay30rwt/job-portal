81f576b6a32cd298ca6212545fd2ce1db8827e63
akshay30rwt <akshayrwt8700@gmail.com>
2026-07-17 23:00:50 +0530

    feat: add new fields in user model

--- models/User.js
+++ models/User.js
@@ -13,8 +13,28 @@ const userSchema = new mongoose.Schema({
     password: {
         type: String,
         required: true
+    },
+    avatar: {
+        url: { type: String, default: ''},
+        publicId: { type: String, default: '' }
+    },
+    isVerified: {
+        type: Boolean,
+        default: false
+    },
+    verificationToken: {
+        type: String,
+        default: null
+    },
+    resetPasswordToken: {
+        type: String,
+        default: null
+    },
+    resetPasswordExpiry: {
+        type: Date,
+        default: null
     }
-});
+}, { timestamps: true });
 
 const User = mongoose.model('User', userSchema);
 
